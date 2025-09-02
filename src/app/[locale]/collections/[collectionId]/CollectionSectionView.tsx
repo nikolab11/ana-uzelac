4c694256d3e5f216772339c44161cbf3b25d1230
lazar_milosavljevic <lazar.milosavljevic@fincredible.io>
2025-09-02 16:18:32 +0200

    Added side images

--- src/app/[locale]/collections/[collectionId]/CollectionSectionView.tsx
+++ src/app/[locale]/collections/[collectionId]/CollectionSectionView.tsx
@@ -2,6 +2,7 @@ import { CollectionSection } from '@/types/api.types';
 import { useLocale } from 'next-intl';
 import { LocaleType } from '@/types/routing';
 import { SectionImages } from '@/app/[locale]/collections/[collectionId]/SectionImages';
+import Image from 'next/image';
 
 interface Props {
 	section: CollectionSection;
@@ -32,6 +33,18 @@ export function CollectionSectionView(props: Props) {
 				<div className={'grow'}>
 					<SectionImages section={props.section} />
 				</div>
+			</div>
+			<div className={'flex gap-5 pt-9 overflow-x-auto'} style={{
+				msOverflowStyle: 'none',
+				scrollbarWidth: 'none'
+			}}>
+				{
+					props.section.sideImages.map((image, index) => {
+						return (
+							<Image key={index} alt={'saffs'} src={image} width={250} height={300} />
+						);
+					})
+				}
 			</div>
 		</div>
 	);
